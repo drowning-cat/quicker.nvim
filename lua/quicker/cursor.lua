6098d0eb9d6f8d8f2169fc7e2d51a9665e6b46f3
Drowning Cat <shamal.ma.personal@gmail.com>
2025-01-20 00:58:18 +0300

    ??? remove scheduling ???

--- lua/quicker/cursor.lua
+++ lua/quicker/cursor.lua
@@ -14,31 +14,47 @@ local function constrain_cursor()
   end
 end
 
+M.l_constrain_cursor = constrain_cursor
+
+---Utility that checks if event(-s) is already defined
+---@param aug integer|string
+---@param bufnr integer|integer[]
+---@param ... string|string[]
+local event_defined = function(aug, bufnr, ...)
+  return #vim.api.nvim_get_autocmds({
+    group = aug,
+    buffer = bufnr,
+    event = { ... },
+  }) > 0
+end
+
 ---@param bufnr number
 function M.constrain_cursor(bufnr)
-  -- HACK: we have to defer this call because sometimes the autocmds don't take effect.
-  vim.schedule(function()
-    if not vim.api.nvim_buf_is_valid(bufnr) then
-      return
-    end
-    local aug = vim.api.nvim_create_augroup("quicker", { clear = false })
-    vim.api.nvim_create_autocmd("InsertEnter", {
-      desc = "Constrain quickfix cursor position",
-      group = aug,
-      nested = true,
-      buffer = bufnr,
-      -- For some reason the cursor bounces back to its original position,
-      -- so we have to defer the call
-      callback = vim.schedule_wrap(constrain_cursor),
-    })
-    vim.api.nvim_create_autocmd({ "CursorMoved", "ModeChanged" }, {
-      desc = "Constrain quickfix cursor position",
-      nested = true,
-      group = aug,
-      buffer = bufnr,
-      callback = constrain_cursor,
-    })
-  end)
+  if not vim.api.nvim_buf_is_valid(bufnr) then
+    return
+  end
+  local aug = vim.api.nvim_create_augroup("quicker", { clear = false })
+  if event_defined(aug, bufnr, "InsertEnter", "CursorMoved", "ModeChanged") then
+    return
+  end
+  vim.api.nvim_create_autocmd("InsertEnter", {
+    desc = "Constrain quickfix cursor position",
+    group = aug,
+    nested = true,
+    buffer = bufnr,
+    callback = function()
+      constrain_cursor()
+    end,
+  })
+  vim.api.nvim_create_autocmd({ "CursorMoved", "ModeChanged" }, {
+    desc = "Constrain quickfix cursor position",
+    nested = true,
+    group = aug,
+    buffer = bufnr,
+    callback = function()
+      constrain_cursor()
+    end,
+  })
 end
 
 return M
